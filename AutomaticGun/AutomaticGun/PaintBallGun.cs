7cabede7ebe9ba58484bb321e115fa6522ba5b0c
Dmytro Kulinich <spo1091@i.ua>
2023-10-13 11:36:31 +0300

    Add proj

---
+++ AutomaticGun/AutomaticGun/PaintBallGun.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticGun
+{
+    public class PaintBallGun
+    {
+        public PaintBallGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if(!loaded) Reload();
+        }
+        public int MagazineSize { get; private set; } = 16;
+        private int balls = 16;
+
+        public int BallsLoaded { get; private set; }
+
+        public int Balls
+        {
+            get { return balls; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    balls = value;
+                    Reload();
+                }
+            }
+        }
+
+        public bool IsEmty() { return BallsLoaded == 0; }
+
+        public void Reload()
+        {
+            if(balls > MagazineSize)
+            {
+                BallsLoaded = MagazineSize;
+            }
+            else BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if(BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ AutomaticGun/AutomaticGun/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AutomaticGun;
+
+int numberOfBalls = ReadInt(20, "Кiлькiсть патронiв");
+int magazineSize = ReadInt(18, "Розмiр магазину");
+Console.WriteLine($"магазин [false]: ");
+bool.TryParse(Console.ReadLine(), out bool isLoaded);
+PaintBallGun gun = new PaintBallGun(numberOfBalls, magazineSize, isLoaded);
+
+
+while (true)
+{
+    Console.WriteLine($"Всього {gun.Balls} патронiв, в магазинi {gun.BallsLoaded} патронiв.");
+    if (gun.IsEmty())
+    {
+        Console.WriteLine($"МАГАЗИН ПОРОЖНIЙ!");
+    }
+    Console.WriteLine($"Пробiл для пострiлу, r для перезарядки, + щоб додати патронiв, q для виходу.");
+    char key = Console.ReadKey(true).KeyChar;
+
+    if (key == ' ')
+    {
+        Console.WriteLine($"Пострiл {gun.Shoot()}");
+        Console.Clear();
+    }
+    if (key == 'r')
+    {
+        gun.Reload();
+        Console.Clear();
+    }
+    if(key == '+')
+    {
+        gun.Balls += gun.MagazineSize;
+        Console.Clear();
+    }
+    if(key== 'q')
+    {
+        return;
+    }
+}
+static int ReadInt(int lastUsedValue, string prompt)
+{
+    Console.WriteLine(prompt + " " + "[" + lastUsedValue + "]:");
+    string answer = Console.ReadLine();
+    if (int.TryParse(answer, out int resut))
+    {
+        Console.WriteLine("   using value " + resut);
+        return resut;
+    }
+    else
+    {
+        Console.WriteLine("   using value " + lastUsedValue);
+        return lastUsedValue;
+    }
+}
